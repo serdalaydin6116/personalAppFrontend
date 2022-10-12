7106653e7272fd5d58343792a1b741975494c66a
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-12 22:16:23 +0300

    home, login , register were synched with context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -7,10 +7,11 @@ export const AuthContext = createContext();
 const url = "http://127.0.0.1:8000/"
 
 const AuthContextProvider = (props)=>{
-  const [currentUser,setCurrentUser] = useState(false);
-  const [myKey,setMyKey] = useState("")
+  const [currentUser,setCurrentUser] = useState(sessionStorage.getItem('username') || false);
+  let keys = sessionStorage.getItem('token')
+  const [myKey,setMyKey] = useState(keys && window.atob(keys))
 
-  const createUser = async (email,password,firstName,lastName,userName)=>{
+  const createUser = async (email,password,firstName,lastName,userName,navigate)=>{
     try {
       const res = await axios.post(`${url}users/auth/register/`,{
           "username": userName,
@@ -29,40 +30,42 @@ const AuthContextProvider = (props)=>{
         const myToken = window.btoa(res.data.token)
         sessionStorage.setItem('token',myToken)
         toastSuccessNotify('User registered successfully.')
-
+        navigate("/home")
       }
 
       
     } catch (error) {
-
+      console.log(error)
     }
   }
 
-  const signIn = async (email,password,userName)=>{
+  const signIn = async (email,password,userName,navigate)=>{
     try {
-
+      console.log(email)
       const res = await axios.post(`${url}users/auth/login/`,{
         "email": email,
         "username":userName,
         "password":password
       })
-      if(res.data.token){
+      console.log(res)
+      if(res.data.key){
         console.log(res)
-        setMyKey(res.data.token)
+        setMyKey(res.data.key)
         setCurrentUser(res.data.user.username)
         sessionStorage.setItem('username',res.data.user.username)
-        const myToken = window.btoa(res.data.token)
+        const myToken = window.btoa(res.data.key)
         sessionStorage.setItem('token',myToken)
         toastSuccessNotify('User login successfully.')
+        navigate("/home")
       }
 
       
     } catch (error) {
-      
+      console.log(error)
     }
   }
 
-  const logOut = async ()=>{
+  const logOut = async (navigate)=>{
     try {
       var config = {
         method: 'post',
@@ -72,10 +75,13 @@ const AuthContextProvider = (props)=>{
         }
       };
       const res = await axios(config)
+      console.log(res)
       if (res.status === 200) {
         setCurrentUser(false)
         setMyKey(false)
         sessionStorage.clear()
+        toastSuccessNotify('User log out successfully.')
+        navigate("/")
       }
       
       
@@ -83,7 +89,6 @@ const AuthContextProvider = (props)=>{
       
     }
   }
-
 
  let value = {
     createUser,
