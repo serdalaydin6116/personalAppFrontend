61a2a8b056838056171492f93645bb1f99c22a68
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-12 21:16:13 +0300

    updated

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,59 +1,52 @@
+import { createContext, useState } from "react";
 import axios from 'axios';
-import {createContext, useState} from 'react'
+import toastSuccessNotify from 
+ 
 
+export const AuthContext = createContext();
 
+const url = "http://127.0.0.1:8000/"
 
-export  const AuthContext =createContext();
+const AuthContextProvider= (props) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  const [myKey, setMyKey] = useState("");
 
-const url="http://127.0.0.1:8000/"
-const AuthContextProvider=(props)=>{
-  const[currentUser, setCurrentUser]=useState(false);
-  const[myKey, setMyKey]=useState("")
-
-  const createUser=async(email, password,firstName, lastName, userName )=>{
+  const createUser = async (email, password, firstName, lastName, userName) => {
     try {
-      const res= await axios.post(`${url}users/auth/register/`, {
-        "username":userName,
+      const res = await axios.post(`${url}users/auth/register`, {
+        "username": userName,
         "email": email,
         "first_name": firstName,
-        "last_name":lastName,
-        "password":password,
-        "password2":password
-
+        "last_name": lastName,
+        "password": password,
+        "password2": password,
       })
+
       if(res.data.token){
+        console.log(res)
         setMyKey(res.data.token)
         setCurrentUser(res.data.userName)
-        sessionStorage.setItem('username',res.data.userName)
+        sessionStorage.setItem('username', res.data.userName)
+        const myToken = window.btoa(res.data.token)
+        sessionStorage.setItem('token', myToken)
+        toastSuccessNotify('User registered successsfully')
+
 
       }
-
-
     } catch (error) {
-
     }
-
   }
-let value={
-  createUser,
-  currentUser,
-  myKey
-}
+  let value= {
+    createUser,
+    currentUser,
+    myKey,
+  }
 
-
-
-
-
-
-  return(
-    <AuthContext.Provider  value={value}>
+  return (
+    <AuthContext.Provider value={value}>
       {props.children}
-
     </AuthContext.Provider>
   )
-
-
-
-
 }
+
 export default AuthContextProvider;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -13,12 +13,14 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from "react-router-dom";
 import {Formik,Form} from 'formik';
 import * as Yup from 'yup';
+import { AuthContext } from '../contexts/AuthContext';
 
 
 const theme = createTheme();
 
 export default function Register() {
   const navigate = useNavigate()
+  const{createUser}=useContext(AuthContext)
 
 
   return (
@@ -60,6 +62,7 @@ export default function Register() {
         onSubmit={(values,actions)=>{
           actions.resetForm()
           actions.setSubmitting(false)
+          createUser
         }}
         >
           {({values,handleChange,errors,touched,handleBlur})=>(
