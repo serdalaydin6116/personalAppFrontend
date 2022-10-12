b2ad4d70d49a818c7e55fc53a2d988ae918ac765
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-10-12 20:45:41 +0300

    auth context was written

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import axios from 'axios';
+import {createContext, useState} from 'react'
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
+
+
+export  const AuthContext =createContext();
+
+const url="http://127.0.0.1:8000/"
+const AuthContextProvider=(props)=>{
+  const[currentUser, setCurrentUser]=useState(false);
+  const[myKey, setMyKey]=useState("")
+
+  const createUser=async(email, password,firstName, lastName, userName )=>{
+    try {
+      const res= await axios.post(`${url}users/auth/register/`, {
+        "username":userName,
+        "email": email,
+        "first_name": firstName,
+        "last_name":lastName,
+        "password":password,
+        "password2":password
+
+      })
+      if(res.data.token){
+        setMyKey(res.data.token)
+        setCurrentUser(res.data.userName)
+        sessionStorage.setItem('username',res.data.userName)
+
+      }
+
+
+    } catch (error) {
+
+    }
+
+  }
+let value={
+  createUser,
+  currentUser,
+  myKey
 }
 
-export default AuthContext
+
+
+
+
+
+  return(
+    <AuthContext.Provider  value={value}>
+      {props.children}
+
+    </AuthContext.Provider>
+  )
+
+
+
+
+}
+export default AuthContextProvider;
